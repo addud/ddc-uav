b3c2496ed99f8419f445190d8db3021eda7e083f
egementaskin <egementaskin@7e6d32f1-2587-e335-c36e-295ee1374fac>
2012-05-20 01:29:03 +0000

    git-svn-id: https://mkuav.googlecode.com/svn/trunk@7 7e6d32f1-2587-e335-c36e-295ee1374fac

--- src/uav/xml/parserconstructor/Parser.java
+++ src/uav/xml/parserconstructor/Parser.java
@@ -18,35 +18,22 @@ import uav.nc.usb.NCWayPoint;
 import android.os.Environment;
 
 public class Parser {
-	private ArrayList<NCWayPoint> pointList;
+	private List<NCWayPoint> pointList;
 	public Parser()
 	{
 		pointList = new ArrayList<NCWayPoint>();
 	}
-	public ArrayList<NCWayPoint> parse() throws XmlPullParserException, IOException
+	public NCWayPoint[] parse() throws XmlPullParserException, IOException
 	{
 		
 		/*NCWayPoint[] wp_list; = { new NCWayPoint(598412841, 176488838, 1500),
 				new NCWayPoint(598412856, 176489507, 1500) };*/	
 	
 
-<<<<<<< .mine
     XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
     factory.setNamespaceAware(true);
     XmlPullParser xpp = factory.newPullParser();
-=======
-	// 05-16 18:07:58.646: D/NCSerialProtocol(616): rx current position:
-	// 598411852 , 176454225
-	// 05-16 18:10:51.166: D/NCSerialProtocol(733): rx current position:
-	// 598412117 , 176460251
-	// 05-16 18:11:26.726: D/NCSerialProtocol(733): rx current position:
-	// 598414271 , 176454996
 
-	public ArrayList<NCWayPoint> parse() throws XmlPullParserException,
-			IOException {
->>>>>>> .r5
-
-<<<<<<< .mine
     /*
     xpp.setInput( new StringReader (
    		 "<coordinates>" +
@@ -144,54 +131,8 @@ public class Parser {
      }
      eventType = xpp.next();
     }
-    return pointList;
-=======
-		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-		factory.setNamespaceAware(true);
-		XmlPullParser xpp = factory.newPullParser();
-		xpp.setInput(new StringReader(
-				"<coordinates>"
-						+ "<coordinate><id>0</id><lat>59.84118520000000</lat><lng>17.645422500000000</lng><picture/><data/></coordinate>"
-						+ "<coordinate><id>0</id><lat>59.84121170000000</lat><lng>17.646025100000000</lng><picture/><data/></coordinate>"
-						+ "<coordinate><id>0</id><lat>59.84142710000000</lat><lng>17.645499600000000</lng><picture/><data/></coordinate>"
-						+ "<coordinate><id>0</id><lat>59.84118540000000</lat><lng>17.645422400000000</lng><picture/><data/></coordinate>"
-						+ "<coordinate><id>0</id><lat>59.84121170000000</lat><lng>17.646025100000000</lng><picture/><data/></coordinate>"
-						+ "</coordinates>"));
-
-		// xpp.setInput(new StringReader (getXMLfromSD()));
-		int eventType = xpp.getEventType();
-		String type = "";
-		NCWayPoint wyp = null;
-		while (eventType != XmlPullParser.END_DOCUMENT) {
-			if (eventType == XmlPullParser.START_DOCUMENT) {
-			} else if (eventType == XmlPullParser.START_TAG) {
-				if (xpp.getName().equalsIgnoreCase("coordinate")) {
-					type = "coordinate";
-					wyp = new NCWayPoint(0, 0, 1000);
-				} else if (xpp.getName().equalsIgnoreCase("lat")) {
-					type = "lat";
-				} else if (xpp.getName().equalsIgnoreCase("lng")) {
-					type = "lng";
-				}
-			} else if (eventType == XmlPullParser.TEXT) {
-				if (type.equalsIgnoreCase("lat")) {
-					// K=K+Integer.valueOf(xpp.getText().toString().replace(".",
-					// "").trim().substring(0, 9))+",";
-					wyp.setLat(Integer.valueOf(xpp.getText().toString()
-							.replace(".", "").trim().substring(0, 9)));
-				} else if (type.equalsIgnoreCase("lng")) {
-					// K=K+Integer.valueOf(xpp.getText().toString().replace(".",
-					// "").trim().substring(0, 9))+",";
-					wyp.setLon(Integer.valueOf(xpp.getText().toString()
-							.replace(".", "").trim().substring(0, 9)));
-
-					pointList.add(wyp);
-				}
-			}
-			eventType = xpp.next();
-		}
-		return pointList;
->>>>>>> .r5
+    NCWayPoint[] wp_array = new NCWayPoint[pointList.size()];
+    return pointList.toArray(wp_array);
 	}
 	
 	private String getXMLfromSD()
